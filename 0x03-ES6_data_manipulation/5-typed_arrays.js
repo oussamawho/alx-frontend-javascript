7affcf679e68c623a52e8f9e35ee6249fe85253d
oussamawho <osmwho@gmail.com>
2024-10-08 23:14:19 +0000

    x

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,11 @@
-/* eslint-disable */
 export default function createInt8TypedArray(length, position, value) {
-  const buffer = new ArrayBuffer(length);
-  const view = new DataView(buffer, 0);
+  const arr = new ArrayBuffer(length);
+  const val = new DataView(arr);
 
-  if (position > length - 1) {
+  try {
+    val.setInt8(position, value);
+  } catch (e) {
     throw Error('Position outside range');
-  } else {
-    view.setInt8(position, value);
   }
-  return view;
+  return val;
+}
